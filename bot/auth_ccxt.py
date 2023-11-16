e2a6a84be9a29cfc81c848d16edcf973d21e3512
Kurt <17579556+kurt213@users.noreply.github.com>
2023-11-16 08:52:38 +0800

    added market currency pairs logic

--- bot/auth_ccxt.py
+++ bot/auth_ccxt.py
@@ -1,6 +1,8 @@
 import ccxt
 from dotenv import load_dotenv
 import os
+import pickle
+from datetime import datetime
 
 class ConnectCoinbase():
 
@@ -9,27 +11,68 @@ class ConnectCoinbase():
         load_dotenv()
         self.api_key = os.getenv('COINBASE_API_KEY')
         self.api_secret = os.getenv('COINBASE_API_SECRET')
-    
-    def connect(self):
 
         exchange_class = getattr(ccxt, 'coinbase')
-        exchange = exchange_class({
+        self.exchange = exchange_class({
             'apiKey': self.api_key,
             'secret': self.api_secret,
             'enableRateLimit': True,  # this option enables built-in rate limit
         })        
 
         # Check if the exchange is authenticated
-        if not exchange.checkRequiredCredentials():
+        if not self.exchange.checkRequiredCredentials():
             print("API credentials are incorrect or not set properly.")
             exit(1)
+        
+        else:
+            print("API credentials verified successfully")
 
-        # Now you can access Coinbase Pro API methods, for example, fetching your balance:
-        balance = exchange.fetch_balance()
-        print(balance)
+        self.current_datetime = datetime.utcnow()
+        print("current UTC time is: {}".format(self.current_datetime))
+    
+    def get_balance(self):
 
+        balance = self.exchange.fetch_balance()
+        total_all_currencies = balance['total']
+
+        for currency in total_all_currencies:
+            print(currency, balance[currency])
+
+    def get_markets(self, currency_pairs=None):
+
+        if currency_pairs is None:
+            print('No currency pairs provided, using default')
+            currency_pairs = [
+                'ETH/GBP',
+                'BTC/GBP'
+            ]
+
+        self.exchange.load_markets()
+
+        output_dict = {}
+
+        for pair in currency_pairs:
+            market_pair = self.exchange.markets[pair]
+
+            output_dict[market_pair['symbol']] = {
+                    'id': market_pair['id'],
+                    'price': market_pair['info']['price'],
+                    'base': market_pair['base'],
+                    'quote': market_pair['quote']
+                }
+        
+        print(output_dict)
+            
+        
 if __name__ == '__main__':
+
+    currency_pairs = [
+        'ETH/GBP',
+        'BTC/GBP'
+    ]
 
     #print(ccxt.coinbasepro().describe())
     coinbase = ConnectCoinbase()
-    coinbase.connect()
+    #coinbase.get_balance()
+    coinbase.get_markets(currency_pairs)
+    #print(dir(ccxt.coinbase()))
